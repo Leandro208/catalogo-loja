d43af34a4663c7172720f0202ee19b062383be14
Leandro Barbosa <boyleandro_208@hotmail.com>
2022-01-31 19:43:30 -0300

    first commit

---
+++ src/main/java/com/catalogoweb/CatalogoLoja/CatalogoLojaApplication.java
@@ -0,0 +1,13 @@
+package com.catalogoweb.CatalogoLoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalogoweb/CatalogoLoja/CatalogoLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogoweb.CatalogoLoja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
