2caa458d4bfe732e7c8af336cd19f532d68df166
Leandro Barbosa <boyleandro_208@hotmail.com>
2022-03-14 22:21:50 -0300

    update form

---
+++ src/main/resources/static/js/cadastro.js
@@ -0,0 +1,30 @@
+function previewImage() {
+			var file = document.getElementById("file").files;
+			if (file.length > 0) {
+				var fileReader = new FileReader();
+
+				fileReader.onload = function(event) {
+					document.getElementById("preview").setAttribute("src",
+							event.target.result);
+				};
+
+				fileReader.readAsDataURL(file[0]);
+			}
+		}
+		
+		
+		function formatarNumero(){
+			
+			
+			var num = document.getElementById('conv');
+			var inp = num.value;
+				
+				
+				if(event.keyCode == 8){
+				var tamanho = inp.toString().lenght;
+				alert(tamanho)
+				}
+				
+				var x = inp.replace(',00', '');
+				num.value = x + ',00';
+		}
